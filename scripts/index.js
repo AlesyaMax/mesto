aec52cf83c69b7bdf5d60f23eabea1f06bb6420c
Alesya Maximenko <a-lesya.net@mail.ru>
2023-08-03 11:52:50 +0300

    create popup for new place

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 const popupForm = document.querySelector('.popup__edit');
-const popupArea = document.querySelector('.popup');
+const popupArea = document.querySelector('.popup_edit-profile');
 const editButton = document.querySelector('.profile__edit');
-const saveButton = popupForm.querySelector('.popup__save');
-const closeButton = popupArea.querySelector('.popup__close');
+const saveProfileButton = popupForm.querySelector('.popup__save');
+const closeProfileButton = popupArea.querySelector('.popup__close');
 const elements = document.querySelector('.elements');
 const likeButton = elements.querySelectorAll('.elements__like');
 
@@ -10,6 +10,10 @@ const inputName = popupForm.querySelector('.popup__input_type_name');
 const inputDescription = popupForm.querySelector('.popup__input_type_description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
+
+const popupNew = document.querySelector('.popup_add-place');
+const addButton = document.querySelector('.profile__add');
+const closeNewButton = popupNew.querySelector('.popup__close');
 
 const showForm = function() {
   popupArea.classList.add('popup_opened');
@@ -21,6 +25,14 @@ const closeForm = function() {
   popupArea.classList.remove('popup_opened')
 };
 
+const showNewPlaceForm = function() {
+  popupNew.classList.add('popup_opened')
+};
+
+const closeNewPlaceForm = function() {
+  popupNew.classList.remove('popup_opened');
+};
+
 const changeInfo = function(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
@@ -29,5 +41,10 @@ const changeInfo = function(evt) {
 };
 
 editButton.addEventListener('click', showForm);
-closeButton.addEventListener('click', closeForm);
+closeProfileButton.addEventListener('click', closeForm);
 popupForm.addEventListener('submit', changeInfo);
+addButton.addEventListener('click', showNewPlaceForm);
+addButton.addEventListener('click',showNewPlaceForm);
+closeNewButton.addEventListener('click', closeNewPlaceForm);
+
+
