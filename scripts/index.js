6ed7a9012e904e8adc966b0be4e9da12ca2d9f35
Alesya Maximenko <a-lesya.net@mail.ru>
2023-08-10 20:36:35 +0300

    write js for card's buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
+// Переменные для об
 const popupForm = document.querySelector('.popup__edit');
 const popupArea = document.querySelector('.popup_edit-profile');
 const editButton = document.querySelector('.profile__edit');
 const saveProfileButton = popupForm.querySelector('.popup__save');
 const closeProfileButton = popupArea.querySelector('.popup__close');
 const elements = document.querySelector('.elements');
-const likeButton = elements.querySelectorAll('.elements__like');
 
 const inputName = popupForm.querySelector('.popup__input_type_name');
 const inputDescription = popupForm.querySelector('.popup__input_type_description');
+const inputPlace = document.querySelector('.popup__input_type_place');
+const inputLink = document.querySelector('.popup__input_type_source');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
@@ -15,6 +17,7 @@ const popupNew = document.querySelector('.popup_add-place');
 const addButton = document.querySelector('.profile__add');
 const closeNewButton = popupNew.querySelector('.popup__close');
 
+//Работа с формами
 const showForm = function() {
   popupArea.classList.add('popup_opened');
   inputName.value = profileName.textContent;
@@ -31,6 +34,8 @@ const showNewPlaceForm = function() {
 
 const closeNewPlaceForm = function() {
   popupNew.classList.remove('popup_opened');
+  inputPlace.value = inputPlace.ariaPlaceholder;
+  inputLink.value = inputLink.ariaPlaceholder
 };
 
 const changeInfo = function(evt) {
@@ -44,7 +49,96 @@ editButton.addEventListener('click', showForm);
 closeProfileButton.addEventListener('click', closeForm);
 popupForm.addEventListener('submit', changeInfo);
 addButton.addEventListener('click', showNewPlaceForm);
-addButton.addEventListener('click',showNewPlaceForm);
 closeNewButton.addEventListener('click', closeNewPlaceForm);
+
+
+//Работа с карточками
+const createBtn = popupNew.querySelector('.popup__save');
+const initialCards = [
+  {
+    name: 'Трейлер',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/33b2ce28-f3f7-4a94-a5aa-fdb38e4a4842_600.jpg'
+  },
+  {
+    name: 'У картин есть глаза',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/6d97c3a7-77a7-4261-b55f-655143aa042c_600.jpg'
+  },
+  {
+    name: 'Подмосковье',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/ccf12522-357d-494e-bc21-bd3825347bce_600.jpg'
+  },
+  {
+    name: 'Межгалактическое путешествие',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/24677ea2-69c6-4bff-afa5-ed8e1b5e3c10_600.jpg'
+  },
+  {
+    name: 'Сирена',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/4b2135c3-b0e2-4eb8-ab64-ae5fd8f0d357_600.jpg'
+  },
+  {
+    name: 'Слесарь без головы',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/d8766ee0-663c-4fb0-ad93-facd38876c51_600.jpg'
+  },
+  {
+    name: 'Мастер и мастерица',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/607babd4-9f9b-4dc6-baae-cc42fd4aa188_600.jpg'
+  },
+  {
+    name: 'Самогон Яблочкина',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/8b594b98-fd56-4259-9c40-c8429c6529c2_600.jpg'
+  },
+  {
+    name: 'Джин',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/42e1ddde-147f-4eea-ad2d-5bd5eb387088_600.jpg'
+  },
+    {
+    name: 'Амур',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/25ffd7ff-cdd1-4811-8985-1cfafd895cac_600.jpg'
+  },
+  {
+    name: 'В Ад и обратно',
+    link: 'https://mavecloud.s3mts.ru/storage/podcasts/27c24380-6b67-4f17-9109-78c33728cdcc/images/d82c34e5-136a-4310-8501-23fa65d2c6f6_600.jpg'
+  }
+];
+
+const template = document.querySelector('.card').content;
+
+const deleteCard = (e) => {
+  const el = e.target.closest('.elements__item');
+  el.remove();
+};
+
+const likePhoto = (e) => {
+  const el = e.target.closest('.elements__like');
+  el.classList.toggle('elements__like_active')
+};
+
+const createCards = initialCards.forEach(function (element) {
+    const card = template.cloneNode(true);
+    card.querySelector('.elements__text').textContent = element.name;
+    card.querySelector('.elements__photo').src = element.link;
+    card.querySelector('.elements__photo').alt = element.name;
+    const deleteButton = card.querySelector('.elements__delete');
+    const likeButton = card.querySelector('.elements__like');
+    deleteButton.addEventListener('click', deleteCard);
+    likeButton.addEventListener('click', likePhoto);
+    elements.prepend(card) 
+  });
+
+const addNewCards = (evt) => {
+    evt.preventDefault();
+    const card = template.cloneNode(true);
+    card.querySelector('.elements__text').textContent = inputPlace.value;
+    card.querySelector('.elements__photo').src = inputLink.value;
+    card.querySelector('.elements__photo').alt = inputPlace.value;
+    const deleteButton = card.querySelector('.elements__delete');
+    const likeButton = card.querySelector('.elements__like');
+    deleteButton.addEventListener('click', deleteCard);
+    likeButton.addEventListener('click', likePhoto);
+    elements.prepend(card) 
+    closeNewPlaceForm();
+};
+
+ createBtn.addEventListener('click', addNewCards);
 
 
